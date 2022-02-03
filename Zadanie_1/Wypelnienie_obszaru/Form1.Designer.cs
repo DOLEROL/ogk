4dff5c9395ee31621630ffafac5e36afcfaf071c
Ringo-ui <pawel.lipski123@gmail.com>
2022-02-03 22:07:07 +0100

    Zadanie 1: Wypełnienie obszaru

---
+++ Zadanie_1/Wypelnienie_obszaru/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Wypelnienie_obszaru
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.Reset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(107, 89);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 129);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(78, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Spójność 4";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 152);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(78, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Spójność 8";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 175);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(119, 17);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Kontrola parzystości";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(12, 199);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(75, 23);
+            this.Reset.TabIndex = 4;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(160, 241);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button Reset;
+    }
+}
+
---
+++ Zadanie_1/Wypelnienie_obszaru/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Wypelnienie_obszaru
+{
+    public partial class Form1 : Form
+    {
+        int c = 0;
+        Point[] points = new Point[4];
+        Bitmap bmp;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void changePixel(int x, int y, string color)
+        {
+            if (radioButton1.Checked)
+            {
+                bmp.SetPixel(x, y, Color.Red);
+                if (bmp.GetPixel(x + 1, y).Name == color) changePixel(x + 1, y, color);
+                if (bmp.GetPixel(x - 1, y).Name == color) changePixel(x - 1, y, color);
+                if (bmp.GetPixel(x, y + 1).Name == color) changePixel(x, y + 1, color);
+                if (bmp.GetPixel(x, y - 1).Name == color) changePixel(x, y - 1, color);
+            }
+            else if (radioButton2.Checked)
+            {
+                bmp.SetPixel(x, y, Color.Red);
+
+                if (bmp.GetPixel(x + 1, y).Name == color) changePixel(x + 1, y, color);
+                if (bmp.GetPixel(x - 1, y).Name == color) changePixel(x - 1, y, color);
+                if (bmp.GetPixel(x, y + 1).Name == color) changePixel(x, y + 1, color);
+                if (bmp.GetPixel(x, y - 1).Name == color) changePixel(x, y - 1, color);
+
+                if (
+                    bmp.GetPixel(x + 1, y + 1).Name == color
+                    && bmp.GetPixel(x, y + 1).Name == color
+                    && bmp.GetPixel(x + 1, y).Name == color
+                    ) changePixel(x + 1, y + 1, color);
+                if (
+                    bmp.GetPixel(x - 1, y - 1).Name == color
+                    && bmp.GetPixel(x, y - 1).Name == color
+                    && bmp.GetPixel(x - 1, y).Name == color
+                    ) changePixel(x - 1, y - 1, color);
+                if (
+                    bmp.GetPixel(x + 1, y - 1).Name == color
+                    && bmp.GetPixel(x, y - 1).Name == color
+                    && bmp.GetPixel(x + 1, y).Name == color
+                    ) changePixel(x + 1, y - 1, color);
+                if (
+                    bmp.GetPixel(x - 1, y + 1).Name == color
+                    && bmp.GetPixel(x, y + 1).Name == color
+                    && bmp.GetPixel(x - 1, y).Name == color
+                    ) changePixel(x - 1, y + 1, color);
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (c == 0)
+            {
+                points[0] = new Point(e.X, e.Y);
+                points[3] = points[0];
+                c++;
+            }
+            else if (c == 1)
+            {
+                points[1] = new Point(e.X, e.Y);
+                c++;
+            }
+            else if (c == 2)
+            {
+                points[2] = new Point(e.X, e.Y);
+                bmp = new Bitmap(pictureBox1.Size.Width - 1, pictureBox1.Size.Height - 1);
+                pictureBox1.Image = bmp;
+                Graphics g = Graphics.FromImage(bmp);
+                Pen pen = new Pen(Color.Black, 1);
+                g.DrawLines(pen, points);
+                if (radioButton3.Checked)
+                {
+                    int w, h;
+                    w = pictureBox1.Size.Width - 1;
+                    h = pictureBox1.Size.Height - 1;
+
+                    for (int i = 0; i < h; i++)
+                    {
+                        int n = 0;
+                        for (int j = 0; j < w; j++)
+                        {
+                            if (bmp.GetPixel(j, i).Name != "0" && bmp.GetPixel(j, i).Name != bmp.GetPixel(j - 1, i).Name)
+                            {
+                                n++;
+                            }
+                            else
+                            {
+                                if (n % 2 == 1)
+                                {
+                                    bmp.SetPixel(j, i, Color.Red);
+                                }
+                            }
+                        }
+                    }
+                }
+                c++;
+            }
+            else if (c == 3)
+            {
+                changePixel(e.X, e.Y, bmp.GetPixel(e.X, e.Y).Name);
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            c = 0;
+            bmp = new Bitmap(pictureBox1.Size.Width - 1, pictureBox1.Size.Height - 1);
+            pictureBox1.Image = bmp;
+        }
+    }
+}
