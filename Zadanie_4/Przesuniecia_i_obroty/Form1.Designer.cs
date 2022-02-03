41d124851d3d90ffa0b6ad2bdebcfb03ed7efe81
Ringo-ui <pawel.lipski123@gmail.com>
2022-02-03 22:10:47 +0100

    Zadanie 4: Przesunięcia i obroty

---
+++ Zadanie_4/Przesuniecia_i_obroty/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace Przesuniecia_i_obroty
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(9, 424);
+            this.hScrollBar1.Maximum = 500;
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(632, 17);
+            this.hScrollBar1.TabIndex = 2;
+            this.hScrollBar1.ValueChanged += new System.EventHandler(this.hScrollBar1_ValueChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(626, 383);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.hScrollBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Zadanie_4/Przesuniecia_i_obroty/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Przesuniecia_i_obroty
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        Graphics g;
+        Pen pen;
+        Point[] p1, p2, p3, p4, p5;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pictureBox1.Size.Width - 1, pictureBox1.Size.Height - 1);
+            pictureBox1.Image = bmp;
+            g = Graphics.FromImage(bmp);
+
+            pen = new Pen(Color.Black, 1);
+
+            p1 = new Point[] {
+                new Point(50, 50),
+                new Point(50, 100),
+                new Point(100, 100),
+                new Point(100, 50),
+            };
+            p2 = new Point[] {
+                new Point(200, 50),
+                new Point(200, 100),
+                new Point(250, 100),
+                new Point(250, 50),
+            };
+            p3 = new Point[] {
+                new Point(50, 200),
+                new Point(50, 250),
+                new Point(100, 250),
+                new Point(100, 200),
+            };
+            p4 = new Point[] {
+                new Point(200, 200),
+                new Point(200, 250),
+                new Point(250, 250),
+                new Point(250, 200),
+            };
+            p5 = new Point[] {
+                new Point(125, 125),
+                new Point(125, 175),
+                new Point(175, 175),
+                new Point(175, 125),
+            };
+
+            g.DrawPolygon(pen, p1);
+            g.DrawPolygon(pen, p2);
+            g.DrawPolygon(pen, p3);
+            g.DrawPolygon(pen, p4);
+            g.FillPolygon(Brushes.Red, p5);
+        }
+
+        private void hScrollBar1_ValueChanged(object sender, EventArgs e)
+        {
+            int scroll = hScrollBar1.Value;
+
+            bmp = new Bitmap(pictureBox1.Size.Width - 1, pictureBox1.Size.Height - 1);
+            pictureBox1.Image = bmp;
+            g = Graphics.FromImage(bmp);
+
+            Matrix matrDraw = new Matrix();
+            matrDraw.Reset();
+            matrDraw.Translate(scroll, 0);
+            matrDraw.RotateAt(-scroll * 2, new Point(150, 150));
+
+            g.Transform = matrDraw;
+            g.DrawPolygon(pen, p1);
+            g.DrawPolygon(pen, p2);
+            g.DrawPolygon(pen, p3);
+            g.DrawPolygon(pen, p4);
+
+            g.Dispose();
+            g = Graphics.FromImage(bmp);
+
+            Matrix matrFill = new Matrix();
+            matrFill.Reset();
+            matrFill.Translate(scroll, 0);
+            matrFill.RotateAt(scroll * 2, new Point(150, 150));
+
+            g.Transform = matrFill;
+            g.FillPolygon(Brushes.Red, p5);
+        }
+    }
+}
