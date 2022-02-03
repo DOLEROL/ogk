9c8560dd8cdc1703d6e22381e84308e076a49d13
Ringo-ui <pawel.lipski123@gmail.com>
2022-02-03 22:10:02 +0100

    Zadanie 3: Wypróbowanie wybranych algorytmów rysowania odcinków i okręgów

---
+++ Zadanie_3/proste_i_okregi/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿
+namespace proste_i_okregi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(231, 321);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Linia";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 300);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 321);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "xp=";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 347);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "yp=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(116, 321);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "xk=";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(116, 350);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "yk";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(231, 348);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(54, 17);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Okrąg";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(322, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(259, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Rysuj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(322, 350);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(259, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Rysuj";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(322, 379);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(259, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Rysuj";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(322, 408);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(259, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Rysuj";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(42, 321);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            299,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(53, 20);
+            this.numericUpDown1.TabIndex = 15;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            150,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(42, 347);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            299,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(53, 20);
+            this.numericUpDown2.TabIndex = 16;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            150,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(146, 321);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            299,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(53, 20);
+            this.numericUpDown3.TabIndex = 17;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(146, 348);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(53, 20);
+            this.numericUpDown4.TabIndex = 18;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+    }
+}
+
---
+++ Zadanie_3/proste_i_okregi/Form1.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace proste_i_okregi
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        bool exec = false;
+        int option = 0;
+        int xp, xk, yp, yk, x0, y0;
+        float R;
+        Stopwatch sw = new Stopwatch();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            numericUpDown1.Maximum = panel1.Size.Width;
+            numericUpDown2.Maximum = panel1.Size.Height - 1;
+            numericUpDown3.Maximum = panel1.Size.Width;
+            numericUpDown4.Maximum = panel1.Size.Height-1;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            if (exec)
+            {
+                bmp = new Bitmap(panel1.Size.Width, panel1.Size.Height);
+                e.Graphics.DrawImage(bmp, new Point(0, 0));
+
+                xp = (int)numericUpDown1.Value;
+                yp = (int)numericUpDown2.Value;
+                xk = (int)numericUpDown3.Value;
+                yk = (int)numericUpDown4.Value;
+
+                x0 = (int)numericUpDown1.Value;
+                y0 = (int)numericUpDown2.Value;
+                R = (float)numericUpDown3.Value;
+
+                if (option == 1)
+                {
+                    float dx = xk - xp;
+                    float dy = yk - yp;
+                    float m = dy / dx;
+                    float y = yp;
+
+                    sw.Start();
+
+                    for (int i = xp; i < xk; ++i)
+                    {
+                        bmp.SetPixel(i, (int)Math.Floor(y + 0.5), Color.Black);
+                        y += m;
+                    }
+
+                    sw.Stop();
+                    MessageBox.Show("Czas trwania: " + sw.Elapsed.ToString());
+                }
+                else if (option == 2)
+                {
+                    int dx, dy, incrE, incrNE, d, x, y;
+                    dx = xk - xp;
+                    dy = yk - yp;
+                    d = 2 * dy - dx;
+                    incrE = dy * 2;
+                    incrNE = (dy - dx) * 2;
+                    x = xp;
+                    y = yp;
+
+                    sw.Start();
+
+                    bmp.SetPixel(x, y, Color.Black);
+                    while (x < xk)
+                    {
+                        if (d < 0)
+                        {
+                            d += incrE;
+                            x++;
+                        }
+                        else
+                        {
+                            d += incrNE;
+                            x++;
+                            y++;
+                        }
+                        bmp.SetPixel(x, y, Color.Black);
+                    }
+
+                    sw.Stop();
+                    MessageBox.Show("Czas trwania: " + sw.Elapsed.ToString());
+                }
+                else if (option == 3)
+                {
+                    float dest = (float)(Math.PI / 2);
+                    float step = 1 / R;
+
+                    sw.Start();
+
+                    for (float i = 0; i < dest; i += step)
+                    {
+                        float x = (float)(R * Math.Cos(i));
+                        float y = (float)(R * Math.Sin(i));
+
+                        bmp.SetPixel((int)(x0 + x), (int)(y0 + y), Color.Black);
+                        bmp.SetPixel((int)(x0 - x), (int)(y0 + y), Color.Black);
+                        bmp.SetPixel((int)(x0 + x), (int)(y0 - y), Color.Black);
+                        bmp.SetPixel((int)(x0 - x), (int)(y0 - y), Color.Black);
+                    }
+
+                    sw.Stop();
+                    MessageBox.Show("Czas trwania: " + sw.Elapsed.ToString());
+                }
+                else if (option == 4)
+                {
+                    float dest = (float)(Math.PI / 4);
+                    float step = 1 / R;
+
+                    sw.Start();
+
+                    for (float i = 0; i < dest; i += step)
+                    {
+                        float x = (float)(R * Math.Cos(i));
+                        float y = (float)(R * Math.Sin(i));
+
+                        bmp.SetPixel((int)(x0 + x), (int)(y0 + y), Color.Black);
+                        bmp.SetPixel((int)(x0 - x), (int)(y0 + y), Color.Black);
+                        bmp.SetPixel((int)(x0 + x), (int)(y0 - y), Color.Black);
+                        bmp.SetPixel((int)(x0 - x), (int)(y0 - y), Color.Black);
+
+                        bmp.SetPixel((int)(x0 + y), (int)(y0 + x), Color.Black);
+                        bmp.SetPixel((int)(x0 + y), (int)(y0 - x), Color.Black);
+                        bmp.SetPixel((int)(x0 - y), (int)(y0 + x), Color.Black);
+                        bmp.SetPixel((int)(x0 - y), (int)(y0 - x), Color.Black);
+                    }
+
+                    sw.Stop();
+                    MessageBox.Show("Czas trwania: " + sw.Elapsed.ToString());
+                }
+                else if (option == 5)
+                {
+                    int x, y;
+                    float d;
+                    x = 0;
+                    y = (int)R;
+                    d = 5 / 4 - R;
+
+                    sw.Start();
+
+                    bmp.SetPixel((int)(x0 + x), (int)(y0 + y), Color.Black);
+                    bmp.SetPixel((int)(x0 - x), (int)(y0 + y), Color.Black);
+                    bmp.SetPixel((int)(x0 + x), (int)(y0 - y), Color.Black);
+                    bmp.SetPixel((int)(x0 - x), (int)(y0 - y), Color.Black);
+
+                    bmp.SetPixel((int)(x0 + y), (int)(y0 + x), Color.Black);
+                    bmp.SetPixel((int)(x0 + y), (int)(y0 - x), Color.Black);
+                    bmp.SetPixel((int)(x0 - y), (int)(y0 + x), Color.Black);
+                    bmp.SetPixel((int)(x0 - y), (int)(y0 - x), Color.Black);
+                    while (y > x)
+                    {
+                        if (d < 0)
+                        {
+                            d += x * 2 + 3;
+                            x++;
+                        }else
+                        {
+                            d += (x - y) * 2 + 5;
+                            x++;
+                            y--;
+                        }
+                        bmp.SetPixel((int)(x0 + x), (int)(y0 + y), Color.Black);
+                        bmp.SetPixel((int)(x0 - x), (int)(y0 + y), Color.Black);
+                        bmp.SetPixel((int)(x0 + x), (int)(y0 - y), Color.Black);
+                        bmp.SetPixel((int)(x0 - x), (int)(y0 - y), Color.Black);
+
+                        bmp.SetPixel((int)(x0 + y), (int)(y0 + x), Color.Black);
+                        bmp.SetPixel((int)(x0 + y), (int)(y0 - x), Color.Black);
+                        bmp.SetPixel((int)(x0 - y), (int)(y0 + x), Color.Black);
+                        bmp.SetPixel((int)(x0 - y), (int)(y0 - x), Color.Black);
+                    }
+
+                    sw.Stop();
+                    MessageBox.Show("Czas trwania: " + sw.Elapsed.ToString());
+
+                }
+                else if (option == 6)
+                {
+                    int x, y, d, deltaE, deltaSE;
+                    x = 0;
+                    y = (int)R;
+                    d = 1 - (int)R;
+                    deltaE = 3;
+                    deltaSE = 5 - (int)R * 2;
+
+                    sw.Start();
+
+                    bmp.SetPixel((int)(x0 + x), (int)(y0 + y), Color.Black);
+                    bmp.SetPixel((int)(x0 - x), (int)(y0 + y), Color.Black);
+                    bmp.SetPixel((int)(x0 + x), (int)(y0 - y), Color.Black);
+                    bmp.SetPixel((int)(x0 - x), (int)(y0 - y), Color.Black);
+
+                    bmp.SetPixel((int)(x0 + y), (int)(y0 + x), Color.Black);
+                    bmp.SetPixel((int)(x0 + y), (int)(y0 - x), Color.Black);
+                    bmp.SetPixel((int)(x0 - y), (int)(y0 + x), Color.Black);
+                    bmp.SetPixel((int)(x0 - y), (int)(y0 - x), Color.Black);
+
+                    while (y > x)
+                    {
+                        if (d < 0)
+                        {
+                            d += deltaE;
+                            deltaE += 2;
+                            deltaSE += 2;
+                            x++;
+                        }
+                        else
+                        {
+                            d += deltaSE;
+                            deltaE += 2;
+                            deltaSE += 4;
+                            x++;
+                            y--;
+                        }
+
+                        bmp.SetPixel((int)(x0 + x), (int)(y0 + y), Color.Black);
+                        bmp.SetPixel((int)(x0 - x), (int)(y0 + y), Color.Black);
+                        bmp.SetPixel((int)(x0 + x), (int)(y0 - y), Color.Black);
+                        bmp.SetPixel((int)(x0 - x), (int)(y0 - y), Color.Black);
+
+                        bmp.SetPixel((int)(x0 + y), (int)(y0 + x), Color.Black);
+                        bmp.SetPixel((int)(x0 + y), (int)(y0 - x), Color.Black);
+                        bmp.SetPixel((int)(x0 - y), (int)(y0 + x), Color.Black);
+                        bmp.SetPixel((int)(x0 - y), (int)(y0 - x), Color.Black);
+                    }
+
+                    sw.Stop();
+                    MessageBox.Show("Czas trwania: " + sw.Elapsed.ToString());
+
+                }
+                e.Graphics.DrawImage(bmp, new Point(0, 0));
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label1.Text = "xp=";
+            label2.Text = "yp=";
+            label3.Text = "xk=";
+            label4.Text = "yk=";
+
+            numericUpDown4.Visible = true;
+
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = false;
+            button4.Visible = false;
+            button1.Text = "Inkrementacyjnie";
+            button2.Text = "Punkt środkowy";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Text = "x0=";
+            label2.Text = "y0=";
+            label3.Text = "R=";
+            label4.Visible = false;
+
+            numericUpDown4.Visible = false;
+
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+            button4.Visible = true;
+            button1.Text = "Symetria 4";
+            button2.Text = "Symetria 8";
+            button3.Text = "Punkt środkowy";
+            button4.Text = "Punkt środkowy drugiego rzędu";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                exec = true;
+                option = 1;
+                panel1.Invalidate();
+            }
+            else
+            {
+                exec = true;
+                option = 3;
+                panel1.Invalidate();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                exec = true;
+                option = 2;
+                panel1.Invalidate();
+            }
+            else
+            {
+                exec = true;
+                option = 4;
+                panel1.Invalidate();
+            }
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            exec = true;
+            option = 5;
+            panel1.Invalidate();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            exec = true;
+            option = 6;
+            panel1.Invalidate();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
